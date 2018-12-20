1154d617f5b5b2c4053d7736b88b6828826d51e6
Victor Raul Tobar <victor@victortobar.com>
2018-12-19 16:17:54 -0800

    Setup express server

--- client/app.jsx
+++ client/app.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>App</h1>, document.getElementById('app'));
+ReactDOM.render(<h1>App</h1>, document.getElementById('navigation-bar'));
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = 3003;
+
+// STATIC FILES
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// MIDDLEWARE
+app.use(morgan('dev'));
+
+// ROUTES
+
+// 404
+app.use((req, res) => {
+  res.status(404).send('Not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

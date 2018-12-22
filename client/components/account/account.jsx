7e28390d7f63ec5420563b5d63ec480480622e67
Victor Raul Tobar <victor@victortobar.com>
2018-12-21 16:51:03 -0800

    Setup Ad and Account components layout

--- client/components/account/account.jsx
+++ client/components/account/account.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Account = (props) => {
   return (
-    <div className="v_account-section"></div>
+    <div className="v_account-section">
+      <div className="v_account-section__button">
+        <p className="v_account-section__initial-text">Hello, Sign in</p>
+        <p>Account &amp; Lists</p>
+      </div>
+      <div className="v_account-section__button">Orders</div>
+      <div className="v_account-section__button">Try Prime</div>
+      <div className="v_account-section__button">Cart</div>
+    </div>
   );
 };
 

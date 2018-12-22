0fe9d00c3dbd9220af2adfb7f399c9e4f305a3eb
Victor Raul Tobar <victor@victortobar.com>
2018-12-21 17:21:03 -0800

    Implement Logo and Location component layout

--- client/components/location/location.jsx
+++ client/components/location/location.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Location = (props) => {
   return (
-    <div className="v_location-section"></div>
+    <div className="v_location-section">
+      <div className="v_location-section__image">0</div>
+      <div className="v_location-section__info">
+        <p className="v_location-section__info--initial-text">Deliver to</p>
+        <p className="v_location-section__info--secondary-text">New York 1001</p>
+      </div>
+    </div>
   );
 };
 

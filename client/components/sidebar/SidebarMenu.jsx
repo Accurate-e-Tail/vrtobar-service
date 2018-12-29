22b947dca5f4b05413f0f6ef61e4eeda4ff4bc68
Victor Raul Tobar <victor@victortobar.com>
2018-12-28 20:40:03 -0800

    Implement back button for submenus

--- client/components/sidebar/SidebarMenu.jsx
+++ client/components/sidebar/SidebarMenu.jsx
@@ -2,34 +2,6 @@ import React from 'react';
 
 import SidebarSubMenu from './SidebarSubMenu.jsx';
 import LIST_ITEMS from './menus.json';
-// const LIST_ITEMS = [
-//   {
-//     header: 'SHOP BY CATEGORY',
-//     items: [
-//       'Prime Video', 'Amazon Music', 'Echo & Alexa', 'FireTablets', 'Fire TV',
-//       'Kindle E-readers & Books', 'Appstore for Android',
-//     ],
-//   },
-//   {
-//     items: [
-//       'Clothing, Shoes, Jewelry & Watches', 'AmazonFresh', 'Books & Audible', 'Movies, Music & Games',
-//       'Electronics, Computers & Office', 'Alexa Smart Home', 'Home, Garden & Tools', 'Pet Supplies',
-//       'Restaurants, Food & Grocery', 'Beauty & Health', 'Toys, Kids & Baby', 'Handmade',
-//       'Sports & Outdoors', 'Automotive & Industrial',
-//     ],
-//   },
-//   {
-//     items: [
-//       'Whole Foods Market', 'Make Money with Amazon', 'Home Services', 'Credit & Payment Products',
-//       'Amazon Launchpad', 'Amazon Exclusives', 'Subscribe with Amazon', 'Treasure Truck',
-//       'International Shopping', 'Amazon Photos & Prints', 'Small & Medium Business', 'Full Store Directory',
-//     ],
-//   },
-//   {
-//     header: 'HELP & SETTINGS',
-//     items: ['Your Account', 'English', 'United States', 'Help', 'Sign In'],
-//   },
-// ];
 
 class SidebarMenu extends React.Component {
   constructor(props) {
@@ -40,6 +12,11 @@ class SidebarMenu extends React.Component {
       subMenuItemIndex: null,
     };
     this.listItemClicked = this.listItemClicked.bind(this);
+    this.backClickHandler = this.backClickHandler.bind(this);
+  }
+
+  backClickHandler() {
+    this.setState({ isSubmenuOpen: false });
   }
 
   listItemClicked(e) {
@@ -63,7 +40,10 @@ class SidebarMenu extends React.Component {
     if (this.state.isSubmenuOpen) {
       menuItemsJSX = LIST_ITEMS[this.state.subMenuIndex].items[this.state.subMenuItemIndex].subItems.map((subMenu, index) => {
         return (
-          <SidebarSubMenu listItemClicked={this.listItemClicked} subMenuIndex={index} subMenu={subMenu} />
+          <React.Fragment>
+            <li className="v_sidebar-list__list-back" onClick={this.backClickHandler}>-- Main Menu</li>
+            <SidebarSubMenu listItemClicked={this.listItemClicked} subMenuIndex={index} subMenu={subMenu} />
+          </React.Fragment>
         );
       });
     } else {
@@ -80,7 +60,6 @@ class SidebarMenu extends React.Component {
           <p className="v_sidebar-menu__header--text">Hello. Sign in</p>
         </div>
         <div className="v_sidebar-menu__list">
-          {this.state.isSubmenuOpen && <li className="v_sidebar-list__list-back">-- Main Menu</li>}
           {menuItemsJSX}
         </div>
       </div>
